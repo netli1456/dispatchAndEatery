6e943574139d4269d10315f2f33c2220dc682ce2
weezy1456 <w800548@gmail.com>
2024-09-28 10:05:44 +0100

    now

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'capacitor://localhost', 
-      'https://mbiteapi.onrender.com' 
+      'https://mbitee.onrender.com' 
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
