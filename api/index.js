3523bbbb7b3c9c190f20973f036a3ddcde0e28b8
weezy1456 <w800548@gmail.com>
2024-09-28 09:06:06 +0100

    update

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['https://yourfrontend.onrender.com', 'capacitor://com.mbite.app'],
+    origin: ['https://mbitee.onrender.com', 'capacitor://com.mbite.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
